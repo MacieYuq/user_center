678762d507b31ce4b27b183ee33a8c17c6357f5f
MacieYuq <69189237+MacieYuq@users.noreply.github.com>
2023-05-08 03:13:20 +0200

    Update GlobalExceptionHandler.java

--- src/main/java/com/Macieyuq/usercenter/exception/GlobalExceptionHandler.java
+++ src/main/java/com/Macieyuq/usercenter/exception/GlobalExceptionHandler.java
@@ -1,8 +1,8 @@
-package com.yupi.usercenter.exception;
+package com.Macieyuq.usercenter.exception;
 
-import com.yupi.usercenter.common.BaseResponse;
-import com.yupi.usercenter.common.ErrorCode;
-import com.yupi.usercenter.common.ResultUtils;
+import com.Macieyuq.usercenter.common.BaseResponse;
+import com.Macieyuq.usercenter.common.ErrorCode;
+import com.Macieyuq.usercenter.common.ResultUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
  * 全局异常处理器
  *
- * @author yupi
+ * @author Macieyuq
  */
 @RestControllerAdvice
 @Slf4j
